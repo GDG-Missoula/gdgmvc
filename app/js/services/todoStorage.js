6e036b3ac472de16280f9a308151d8b5bc4889bb
Rob Becker <robrbecker@gmail.com>
2013-04-29 22:41:53 -0600

    Used yeoman to quickly make it a Chrome App

---
+++ app/js/services/todoStorage.js
@@ -0,0 +1,30 @@
+/*global todomvc */
+'use strict';
+
+/**
+ * Services that persists and retrieves TODOs from localStorage
+ */
+todomvc.factory('todoStorage', function () {
+	var STORAGE_ID = 'todos-angularjs';
+	var todosMem = [];
+	if (chrome) return {
+		get: function () {
+			return todosMem;
+		},
+
+		put: function (todos) {
+			todosMem = todos;
+		}
+	};
+
+
+	return {
+		get: function () {
+			return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
+		},
+
+		put: function (todos) {
+			localStorage.setItem(STORAGE_ID, JSON.stringify(todos));
+		}
+	};
+});
